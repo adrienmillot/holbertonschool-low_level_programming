f3e46a7e2eb99b6a42127e084bf41f21ef4e26de
Adrien <adrien.millot@gmail.com>
2021-02-04 11:05:40 +0100

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Begin of execution
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	for (int n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
