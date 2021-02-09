8d1eb55ca2ccb3f20424f5334cfed9436ebc3d62
Adrien <adrien.millot@gmail.com>
2021-02-09 16:41:19 +0100

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,77 @@
+#include "holberton.h"
+
+/**
+ * displayNumber - display a number
+ *
+ * @number: number to show
+ *
+ * Return: int - result of addition
+ */
+void displayNumber(int number)
+{
+	int lastDigit;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		number *= -1;
+	}
+
+	lastDigit = number % 10;
+
+	if (number >= 10)
+	{
+		number /= 10;
+		displayNumber(number);
+	}
+
+	_putchar(lastDigit + '0');
+}
+
+/**
+ * squareTable - display a square table
+ *
+ * @n: square number
+ */
+void squareTable(int n)
+{
+	int column = 0, row = 0, number;
+
+	for (row = 0; row <= n; row++)
+	{
+		for (column = 0; column <= n; column++)
+		{
+			number = column * row;
+
+			if (column > 0 && number >= 0 && number < 10)
+			{
+				_putchar(' ');
+			}
+			if (column > 0 && number >= 0 && number < 100)
+			{
+				_putchar(' ');
+			}
+			displayNumber(number);
+			if (column < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
+
+/**
+ * print_times_table - display n square table
+ *
+ * @n: square table number
+ */
+void print_times_table(int n)
+{
+	if (n <= 15 && n > 0)
+	{
+		squareTable(n);
+	}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -8,6 +8,7 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int a, int b);
+int add(int, int);
 void print_to_98(int n);
 void displayNumber(int number);
+void print_times_table(int n);
