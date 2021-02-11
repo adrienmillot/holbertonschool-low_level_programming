f1fbd99082387b9c4afcad6e3b569ce7384e5c9d
Adrien <adrien.millot@gmail.com>
2021-02-11 01:55:49 +0100

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - checks for a digit (0 through 9).
+ *
+ * @a: number to multiply
+ * @b: number to multiply
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
