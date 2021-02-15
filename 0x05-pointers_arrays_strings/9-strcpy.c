300490823afed3a4b90d1ed918cc37cf6eadeb56
Adrien <adrien.millot@gmail.com>
2021-02-15 16:54:44 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: Size of the string
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (*s != '\0')
+	{
+		size++;
+		s++;
+	}
+
+	return (size);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ *               including the terminating null byte (\0),
+ *               to the buffer pointed to by dest
+ *
+ * @dest: char pointer
+ * @src: char pointer
+ *
+ * Return: char*
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int cLoop, size = _strlen(src);
+
+	for (cLoop = 0; cLoop < size; cLoop++)
+	{
+		dest[cLoop] = src[cLoop];
+	}
+
+	return (dest);
+}
