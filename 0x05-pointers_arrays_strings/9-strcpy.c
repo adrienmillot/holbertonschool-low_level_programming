c182b17fa7323bff9d63166f608e09b85e3ab45e
Adrien <adrien.millot@gmail.com>
2021-02-16 13:49:01 +0100

    Fix \0

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -34,10 +34,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int cLoop, size = _strlen(src);
 
-	for (cLoop = 0; cLoop < size; cLoop++)
+	for (cLoop = 0; cLoop <= size; cLoop++)
 	{
 		dest[cLoop] = src[cLoop];
 	}
+
+	dest[size] = '\0';
 
 	return (dest);
 }
