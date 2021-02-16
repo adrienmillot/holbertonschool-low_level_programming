7e79480adfea2deffc0b19def9a25ba6ce09755f
Adrien <adrien.millot@gmail.com>
2021-02-16 11:57:37 +0100

    Fix line over 80 characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string,
+ *         starting with the first character
  *
  * @str: char pointer
  *
