5f90a88baa0e9376822c2658fe26f3220317cc73
Adrien <adrien.millot@gmail.com>
2021-02-16 13:44:29 +0100

    Fix algorithm

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,26 @@
 #include "holberton.h"
 
 /**
+ * _strlen - returns the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: Size of the string
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (*s != '\0')
+	{
+		size++;
+		s++;
+	}
+
+	return (size);
+}
+
+/**
  * puts2 - prints every other character of a string,
  *         starting with the first character
  *
@@ -10,10 +30,11 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int size = _strlen(str), cLoop;
+
+	for (cLoop = 0; cLoop < size; cLoop = cLoop + 2)
 	{
-		_putchar(*str);
-		str = str + 2;
+		_putchar(str[cLoop]);
 	}
 	_putchar('\n');
 }
