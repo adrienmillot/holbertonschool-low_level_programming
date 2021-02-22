7bed30055dd8cea007ce0c478f5182c7b18d1171
Adrien MILLOT <adrien.millot@gmail.com>
2021-02-22 13:44:41 +0100

    Fix Betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @accept: characters to accept
  *
  * Return: a pointer to the first occurrence of the character c
- * 	   in the string s, or NULL if the character is not found
+ *         in the string s, or NULL if the character is not found
  */
 unsigned int _strspn(char *s, char *accept)
 {
