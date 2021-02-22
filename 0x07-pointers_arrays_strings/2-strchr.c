583bbfc79aa38f7df5173aabfa79ddad4c8aa594
Adrien MILLOT <adrien.millot@gmail.com>
2021-02-22 09:14:10 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+/**
+ * _strchr - locates a character in a string.
+ * 
+ * @s: string where to search
+ * @c: character to search
+ * 
+ * Return: A pointer to the first occurence of c
+ */
+char *_strchr(char *s, char c)
+{
+	while(*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (s);
+}
