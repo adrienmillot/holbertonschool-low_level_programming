cde2eb4783b8c444103bbd27dd13a560ef6dc645
Adrien MILLOT <adrien.millot@gmail.com>
2021-02-22 13:15:44 +0100

    Fix trailing whitespace

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 /**
  * _memcpy - copies n bytes from memory area src to memory area dest
- * 
+ *
  * @dest: pointer to memory area
  * @src: pointer to src memory area
  * @n: number ot bytes to fill
- * 
+ *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
