43a618cfa4cec2b94598067646c2521aec719111
Adrien MILLOT <adrien.millot@gmail.com>
2021-02-22 12:02:32 +0100

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,49 @@
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: Size of the string
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (*s != '\0')
+	{
+		size++;
+		s++;
+	}
+
+	return (size);
+}
+
+
+/**
+ * _strstr - locates a substring.
+ *
+ * @haystack: string where to search
+ * @needle: string to search
+ *
+ * Return: the address to first location
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int haystackLoop, needleLoop, size = _strlen(needle);
+
+	for (haystackLoop = 0, needleLoop = 0; haystack[haystackLoop + needleLoop] != '\0'; needleLoop++)
+	{
+		if (needle[needleLoop] != haystack[haystackLoop + needleLoop])
+		{
+			haystackLoop += needleLoop;
+			needleLoop = 0;
+		}
+
+		if (needleLoop == size - 1)
+		{
+			return (&haystack[haystackLoop]);
+		}
+	}
+
+	return (haystack);
+}
