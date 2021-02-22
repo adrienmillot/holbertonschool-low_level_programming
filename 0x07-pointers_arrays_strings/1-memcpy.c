1af3c0b450ea03e4280b6d9d68379ebb67c30cad
Adrien MILLOT <adrien.millot@gmail.com>
2021-02-22 08:29:53 +0100

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * 
+ * @dest: pointer to memory area
+ * @src: pointer to src memory area
+ * @n: number ot bytes to fill
+ * 
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int cLoop;
+
+	for (cLoop = 0; cLoop < n; cLoop++)
+	{
+		dest[cLoop] = src[cLoop];
+	}
+
+	return dest;
+}
