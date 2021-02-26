dd80738d9e6d3334dd7cd1b5c124e4c200f57290
Adrien MILLOT <adrien.millot@gmail.com>
2021-02-26 12:16:51 +0100

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * @argc: argument's number
+ * @argv: argument's array
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
