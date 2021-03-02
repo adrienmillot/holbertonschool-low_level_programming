5a97850f5fc5f2ffb97b2b76b26f6b02be6c3832
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-02 22:19:57 +0100

    I will show you how great I am

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,151 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: Size of the string
+ */
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (*s != '\0')
+	{
+		size++;
+		s++;
+	}
+
+	return (size);
+}
+
+/**
+ * characterNumber - returns character number
+ *
+ * @str: char pointer
+ *
+ * Return: Size of the string
+ */
+int characterNumber(char *str)
+{
+	if (*str != '\0')
+	{
+		if (*str != ' ')
+		{
+			return ((characterNumber(str + 1)) + 1);
+		}
+		else
+		{
+			return (characterNumber(str + 1));
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * wordNumber - returns word number
+ *
+ * @str: char pointer
+ *
+ * Return: Word number
+ */
+int wordNumber(char *str)
+{
+	if (*str != '\0')
+	{
+		if (*str != ' ' && *(str + 1) == ' ')
+		{
+			return ((wordNumber(str + 1)) + 1);
+		}
+		else
+		{
+			return (wordNumber(str + 1));
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * getWord - returns word
+ *
+ * @prmGlobal: char pointer
+ * @prmOffset: start of the word
+ * @prmSize: length of the word
+ *
+ * Return: Word number
+ */
+char *getWord(char *prmGlobal, int prmOffset, int prmSize)
+{
+	char *word;
+	int cLoop;
+
+	word = malloc(sizeof(char) * prmSize + 1);
+
+	if (word == NULL)
+	{
+		return (NULL);
+	}
+
+	for (cLoop = 0; cLoop < (prmSize); cLoop++)
+	{
+		word[cLoop] = prmGlobal[prmOffset + cLoop];
+	}
+
+	word[cLoop] = '\0';
+
+	return (word);
+}
+
+/**
+ * strtow - splits a string into words
+ *
+ * @str: char pointer
+ *
+ * Return: word array
+ */
+char **strtow(char *str)
+{
+	int cLoop = 0, cLoop1 = 0, size, wordSize, word_number;
+	char *word;
+	char **words;
+
+	size = _strlen(str);
+	word_number = wordNumber(str);
+
+	words = malloc(sizeof(char *) * (word_number + 1));
+
+	if (words == NULL || str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (cLoop = 0; cLoop < size && cLoop1 < word_number; cLoop++)
+	{
+		if (str[cLoop] != ' ')
+		{
+			wordSize++;
+		}
+		else
+		{
+			if (wordSize > 0)
+			{
+				word = getWord(str, cLoop - wordSize, wordSize);
+				words[cLoop1] = word;
+				wordSize = 0;
+				cLoop1++;
+			}
+		}
+	}
+	words[cLoop1] = NULL;
+
+	return (words);
+}
