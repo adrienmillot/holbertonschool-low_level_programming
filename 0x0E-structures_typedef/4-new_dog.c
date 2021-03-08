27f52a20e4f3af9ab650241ba1070c27d8028295
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-08 16:52:29 +0100

    Fix free allocution

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -66,12 +66,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = malloc(sizeof(char) * sizeName);
 
 	if (d->name == NULL)
+	{
+		free(d);
 		return (NULL);
+	}
 
 	d->owner = malloc(sizeof(char) * sizeOwner);
 
 	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
 		return (NULL);
+	}
 
 	_strcpy(d->name, name);
 	_strcpy(d->owner, owner);
