433599ebf81edba7575725ec25941f81cd688cc8
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-08 17:25:48 +0100

    Fix reformulation

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
 		if (d->age > 0)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: %s\n", "(nil)");
-		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 	}
 }
