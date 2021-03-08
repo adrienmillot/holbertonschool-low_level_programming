034dfa375f3de3e6ba9be67c85306e65f548de35
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-08 15:31:34 +0100

    Fix nullable structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,10 +9,13 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-	if (d->age > 0)
-		printf("Age: %f\n", d->age);
-	else
-		printf("Age: %s\n", "(nil)");
-	printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		if (d->age > 0)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: %s\n", "(nil)");
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
 }
