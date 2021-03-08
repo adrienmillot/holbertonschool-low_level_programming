6377186406857e72a6d7af605af3c5b120b73182
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-08 16:55:45 +0100

    Fix nullable parameter

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -58,6 +58,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 	int sizeName = _strlen_recursion(name), sizeOwner = _strlen_recursion(owner);
 
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
 	d = malloc(sizeof(struct dog));
 
 	if (d == NULL)
