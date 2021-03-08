683ba606f2b17e0829bebad0c41367ce55e4817e
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-08 16:36:12 +0100

    Fix malloc attribution

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,48 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "dog.h"
+/**
+ * _strlen_recursion - return the length of a string
+ *
+ * @s: char pointer
+ *
+ * Return: the length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (_strlen_recursion(s + 1) + 1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ *               including the terminating null byte (\0),
+ *               to the buffer pointed to by dest
+ *
+ * @dest: char pointer
+ * @src: char pointer
+ *
+ * Return: char*
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int cLoop, size = _strlen_recursion(src);
+
+	for (cLoop = 0; cLoop < size; cLoop++)
+	{
+		dest[cLoop] = src[cLoop];
+	}
+
+	dest[size] = '\0';
+
+	return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -11,19 +53,29 @@
  *
  * Return: a new dog
  */
-dog_t *new_dog(char *name, float age __attribute__((unused)), char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int sizeName = _strlen_recursion(name), sizeOwner = _strlen_recursion(owner);
 
 	d = malloc(sizeof(struct dog));
 
 	if (d == NULL)
-	{
 		return (NULL);
-	}
 
-	d->name = name;
-	d->owner = owner;
+	d->name = malloc(sizeof(char) * sizeName);
+
+	if (d->name == NULL)
+		return (NULL);
+
+	d->owner = malloc(sizeof(char) * sizeOwner);
+
+	if (d->owner == NULL)
+		return (NULL);
+
+	_strcpy(d->name, name);
+	_strcpy(d->owner, owner);
+	d->age = age;
 
 	return (d);
 }
