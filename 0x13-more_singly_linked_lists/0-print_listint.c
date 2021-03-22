da47eda61a3599745d3af52c168cdfd8b8eb7b50
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-22 09:55:36 +0100

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *
+ * @h: list to browse
+ *
+ * Return: size of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (!h)
+		return (0);
+	if (h->next == NULL)
+	{
+		printf("%d\n", h->n);
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", h->n);
+		return (print_listint(h->next) + 1);
+	}
+}
