dee1b09df8bd7f78baf060a90456a4257158a597
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-22 14:56:51 +0100

    Insert

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,6 +15,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+listint_t *createNode(const int prmNbr);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -24,4 +25,5 @@ void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 #endif
