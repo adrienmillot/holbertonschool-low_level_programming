2da440b118372129b6b9c87febab6f734e911100
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-25 14:54:08 +0100

    Fix putchar

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "holberton.h"
 
 /**
  * print_binary - prints the binary representation of a number.
@@ -10,5 +11,5 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 		print_binary(n >> 1);
 
-	printf("%ld", n & 1);
+	_putchar((n & 1) + '0');
 }
