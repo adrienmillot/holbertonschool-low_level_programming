9a0f0226cbd597cf0ea4892a7200c7b5d4080521
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-25 18:00:08 +0100

    Fix error return value

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+/**
+ * count - return binary length
+ *
+ * @n: number
+ *
+ * Return: binary length
+ */
+unsigned int count(unsigned long int n)
+{
+	if (n > 1)
+		return (count(n >> 1) + 1);
+	return (1);
+}
 
 /**
  * get_bit - get a bit value at specific index
@@ -10,5 +22,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int length = count(n);
+
+	if (index > length)
+		return (-1);
 	return ((n >> (index)) & 1);
 }
