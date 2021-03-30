44991edbbc1d234a5971e82ffd87b6ecdbd4c3f0
Adrien MILLOT <adrien.millot@gmail.com>
2021-03-30 11:24:11 +0200

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it
+ *                 to the POSIX standard output.
+ *
+ * @filename: filename
+ * @letters: letter's number
+ *
+ * Return: letter's number
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, w;
+	long int n;
+	char *buf;
+
+	buf = malloc(sizeof(char) * (letters + 1));
+
+	if (!buf)
+		return (0);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	n = read(fd, buf, letters);
+	close(fd);
+	if (n == -1)
+		return (0);
+	buf[letters] = '\0';
+
+	w = write(STDOUT_FILENO, buf, n);
+	free(buf);
+	if (w == -1)
+		return (0);
+
+	return (n);
+}
