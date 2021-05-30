d0ae56df93e0ea2122ba176494528c6f8204061e
Adrien MILLOT <adrien.millot@gmail.com>
2021-05-30 14:52:50 +0200

    key -> index

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,4 +35,8 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int prmSize);
 unsigned long int hash_djb2(const unsigned char *prmString);
+unsigned long int key_index(
+	const unsigned char *prmKey,
+	unsigned long int prmSize
+);
 #endif
