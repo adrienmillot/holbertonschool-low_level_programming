d25a05e1ffdad9d37ea47f8c2dc1048b93b0869d
Adrien MILLOT <adrien.millot@gmail.com>
2021-07-02 15:00:12 +0200

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,22 +14,13 @@ def island_perimeter(grid):
 
     for row in range(len(grid)):
         for column in range(len(grid[row])):
-            if row == 0 or row == len(grid) - 1:
-                peri += 1
-            if column == 0 or column == len(grid[row]) - 1:
-                peri += 1
-            if (grid[row][column] == 1 and
-                    row > 0 and grid[row - 1][column] == 0):
-                peri += 1
-            if (grid[row][column] == 1 and
-                    column > 0 and grid[row][column - 1] == 0):
-                peri += 1
-            if (grid[row][column] == 1 and
-                    column < len(grid[row]) - 1 and
-                    grid[row][column + 1] == 0):
-                peri += 1
-            if (grid[row][column] == 1 and
-                    row < len(grid) - 1 and grid[row + 1][column] == 0):
-                peri += 1
-
+            if grid[row][column] == 1:
+                if row == 0 or grid[row - 1][column] == 0:
+                    peri += 1
+                if column == 0 or grid[row][column - 1] == 0:
+                    peri += 1
+                if row == len(grid) - 1 or grid[row + 1][column] == 0:
+                    peri += 1
+                if column == len(grid[row]) - 1 or grid[row][column + 1] == 0:
+                    peri += 1
     return peri
